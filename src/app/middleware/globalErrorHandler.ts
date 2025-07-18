1ba52fb93646a51a92adda89a129c79843885dc7
Mohammad Ashif <mohammadashif510@gmail.com>
2025-07-18 10:21:53 +0600

    handle error whit AppError done

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
+import config from "../config";
+import { AppError } from "../errorHelpers/AppError";
 
 export const globalErrorHandler = (
   err: any,
@@ -8,9 +10,18 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  res.status(500).json({
-    success: true,
-    message: "Something went wrong",
-    err,
-  });
+  let statusCode = 500;
+  let message = "Something went wrong";
+
+  if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    res.status(statusCode).json({
+      success: false,
+      message,
+      err,
+      stack: config.node_env === "development" ? err.stack : "",
+    });
+  }
 };
--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { UserServices } from "./user.service";
 import httpStatus from "http-status-codes";
+import { AppError } from "../../errorHelpers/AppError";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    throw new Error();
+    throw new AppError(httpStatus.BAD_GATEWAY, "Nothing is impossible");
     const data = req.body;
     const result = await UserServices.createUserIntoDB(data);
     res.status(httpStatus.CREATED).json({
@@ -12,11 +15,6 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       data: result,
     });
   } catch (error) {
-    // res.status(500).json({
-    //   success: false,
-    //   message: `Something went wrong`,
-    //   error,
-    // });
     next(error);
   }
 };

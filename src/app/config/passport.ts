effa94d8b24d7d21cfd60be7e0c04d8c558786cb
Mohammad Ashif <mohammadashif510@gmail.com>
2025-07-22 19:03:19 +0600

    credential authentication with passport

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -8,7 +8,127 @@ import {
 import config from ".";
 import { User } from "../module/user/user.model";
 import { Role } from "../module/user/user.interface";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
 
+// ! passport local login
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email: string, password: string, done) => {
+      try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const isGoogleAuthenticated = user.auths.some(
+          (e) => e.provider == "google",
+        );
+        if (isGoogleAuthenticated) {
+          return done(null, false, {
+            message: "User authenticate through google. If you want to login with credentials please set a password at first.",
+          });
+        }
+
+        const isPasswordMatch = bcrypt.compare(
+          password,
+          user.password as string,
+        );
+        if (!isPasswordMatch) {
+          return done(null, false, { message: "Password does not match" });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
+    },
+  ),
+);
+
+// ! passport for local
+// passport.use(
+//   new LocalStrategy(
+//     {
+//       usernameField: "email",
+//       passwordField: "password",
+//     },
+//     async (email: string, password: string, done) => {
+//       try {
+//         const user = await User.findOne({ email });
+
+//         if (!user) return done(null, false, { message: "User not found" });
+
+//         const isGoogleAuthenticated = user.auths.some(
+//           (e) => e.provider == "google",
+//         );
+//         if (isGoogleAuthenticated) {
+//           return done(null, false, {
+//             message:
+//               "You have authenticated through google. Please set a password and then login with credentials",
+//           });
+//         }
+
+//         const isMatch = await bcrypt.compare(
+//           password as string,
+//           user.password as string,
+//         );
+
+//         if (!isMatch) {
+//           done(null, false, { message: "Password dose not match" });
+//         }
+//         return done(null, user);
+//       } catch (error) {
+//         console.log(error);
+//         done(error);
+//       }
+//     },
+//   ),
+// );
+
+// ! practice credential
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email: string, password: string, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const isGoogleAuthenticated = user.auths.some(
+          (e) => e.provider == "google",
+        );
+        if (isGoogleAuthenticated) {
+          return done(null, false, {
+            message: "You have authenticated with google",
+          });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password as string);
+        if (!isMatch) {
+          return done(null, false, { message: "Password dose not match" });
+        }
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    },
+  ),
+);
+
+// ! passport for google
 passport.use(
   new GoogleStrategy(
     {
--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import {  Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { AuthServices } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
@@ -9,10 +9,12 @@ import { createUserTokens } from "../../utils/userTokens";
 import { AppError } from "../../errorHelpers/AppError";
 import config from "../../config";
 
+
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const loginInfo = await AuthServices.loginUser(req.body);
+  console.log(loginInfo);
   setAuthCookie(res, loginInfo);
-  setAuthCookie(res, loginInfo);
+
 
   sendResponse(res, {
     success: true,
@@ -21,6 +23,8 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
     data: loginInfo,
   });
 });
+
+
 
 const createNewAccessToken = catchAsync(async (req: Request, res: Response) => {
   const refreshToken = req.cookies.refreshToken;
